d0dda4fab445dd281243f41d5cd4ad0d12cf99c1
Aleksandr802 <savs802@gmail.com>
2024-01-26 13:41:45 -0800

    Initial Commit

---
+++ DemoMVVM-C&Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoMVVM-C&Map
+//
+//  Created by Oleksandr Seminov on 1/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
